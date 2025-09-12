67d1f189b4661aa40c8ae07aae7c57f447f0d264
Abiye <abiye@MacBook-Pro.local>
2025-09-12 11:47:04 +0100

    Initial commit

---
+++ src/main/java/org/example/javaexercise2part2/JavaExercise2Part2Application.java
@@ -0,0 +1,13 @@
+package org.example.javaexercise2part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExercise2Part2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExercise2Part2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaexercise2part2/JavaExercise2Part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaexercise2part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExercise2Part2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
